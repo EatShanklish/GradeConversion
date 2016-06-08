cd87dbf88bbea1d26ce90aea706dde099339916c
Hussein Ibrahim <hussein.m.abrahim@gmail.com>
2016-06-08 16:04:12 -0400

    Optimized conditions

--- src/com/Shanklish/GradeConversion/GradeConversion.java
+++ src/com/Shanklish/GradeConversion/GradeConversion.java
@@ -18,43 +18,43 @@ public class GradeConversion {
 			
 			//Multitude of if statements corresponding numerical grades to letter grades
 			
-			if(grade >= 0 && grade <= 60 )
+			if(grade <= 60 )
 				System.out.println("Your letter grade is an F");
 			
-			else if(grade >= 60 && grade <= 62 )
+			else if(grade <= 62 )
 				System.out.println("Your letter grade is a D-");
 			
-			else if(grade >= 63 && grade <= 66 )
+			else if( grade <= 66 )
 				System.out.println("Your letter grade is a D");
 			
-			else if(grade >= 67 && grade <= 69 )
+			else if(grade <= 69 )
 				System.out.println("Your letter grade is a D+");
 			
-			else if(grade >=70 && grade <=72)
+			else if(grade <=72)
 				System.out.println("Your letter grade is a C-");
 			
-			else if(grade >=73 && grade <=76)
+			else if(grade <=76)
 				System.out.println("Your letter grade is a C");
 			
-			else if(grade >=77 && grade <=79)
+			else if(grade <=79)
 				System.out.println("Your letter grade is a C+");
 			
-			else if(grade >=80 && grade <=82)
+			else if(grade <=82)
 				System.out.println("Your letter grade is a B-");
 			
-			else if(grade >=83 && grade <=86)
+			else if( grade <=86)
 				System.out.println("Your letter grade is a B");
 			
-			else if(grade >=87 && grade <=89)
+			else if( grade <=89)
 				System.out.println("Your letter grade is a B+");
 			
-			else if(grade >= 90 && grade <=93)
+			else if( grade <=93)
 				System.out.println("Your letter grade is a A-");
 			
-			else if(grade >= 93 && grade <=97)
+			else if( grade <=97)
 				System.out.println("Your letter grade is a A");
 			
-			else if (grade >= 98 && grade <= 100)
+			else if (grade <= 100)
 				System.out.println("Your letter grade is an A+");
 			
 			
